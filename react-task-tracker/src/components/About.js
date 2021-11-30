848245515d0a1d7545bc0e752403a96ad8e06434
dgallagher <duncang42@gmail.com>
2021-11-30 14:51:02 -0600

    working task tracker with json server

---
+++ react-task-tracker/src/components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div>
+          <p>Simple learning project with React. See more of my stuff at <a href="https://duncangallagher.dev">duncangallagher.dev</a></p>
+          <a href="/">Go Back</a>
+        </div>
+    )
+}
+
+export default About
